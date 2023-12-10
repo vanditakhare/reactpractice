dac679199bcbc2bf216f769951ecc57036922d3a
Vandita Khare <117070063+vanditakhare@users.noreply.github.com>
2023-12-10 17:04:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ function App() {
     <>
       <h1>my series</h1>
       <p>following list consist of the names of top netflix series. do take out the time and dive deep into the realm of these amazing series.</p>
-      <ol>
-        <li>apple</li>
-        <li>affggxgple</li>
-        <li>apple</li>
-        <li>agdgple</li>
-        <li>apple</li>
-      </ol>
+     
     </>
   )
 }
